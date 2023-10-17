a5200c74d5695854b9dd09aa6dd9e0552d6fb239
geeth <geeth@LAPTOP-2S57L5F3>
2023-10-17 22:57:04 +0530

    "First commit"

---
+++ src/test/java/com/LoginApp/MyTest.java
@@ -0,0 +1,25 @@
+package com.LoginApp;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyTest {
+	
+	@Test
+	public void test1()
+	{
+		LoginApp l = new LoginApp();
+		Assert.assertEquals(0, l.validateLogin("abc", "abc@123"));
+	}
+	
+	
+	@Test
+	public void test2()
+	{
+		LoginApp l = new LoginApp();
+		Assert.assertEquals(1, l.validateLogin("geethika", "geethu"));
+		
+	}
+
+
+}
